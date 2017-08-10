fe63a009dafa94318986ebc88798993ff6a78889
Alexandar Kotevski <alexandarkotevski1997@gmail.com>
2017-08-10 18:32:54 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Silverado - Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <h1>Silverado Cinema</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="showing.php">Now Showing</a></li>
+    </ul>
+</nav>
+<main>
+    <p>Grand Re-Opening! December 2017!<br>
+        The entire cinema has been renovated! With comfortable new seats and state of the art 3D projectors, you can watch
+        movies the way they were made to be seen. All theatres now include Dolby Digital Surround Sound, giving you an
+        amazing sounding theatrical experience.</p>
+</main>
+<footer>
+    © 2017 Alexandar Kotevski (s3646348) & Samuel Radford (s3661545)
+</footer>
+</body>
+</html>
---
+++ showing.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Silverado - Now Showing</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <h1>Silverado Cinema</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="showing.php">Now Showing</a></li>
+    </ul>
+</nav>
+<main>
+</main>
+<footer>
+    © 2017 Alexandar Kotevski (s3646348) & Samuel Radford (s3661545)
+</footer>
+</body>
+</html>
